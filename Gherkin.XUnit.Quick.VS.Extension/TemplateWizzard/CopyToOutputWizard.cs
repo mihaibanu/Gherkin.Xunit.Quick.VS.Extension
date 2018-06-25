498a2e655376bb9e45b494044e2e8c88cb91594a
Mihai Banu <mihai.banu@outlook.com>
2018-06-25 17:44:54 +0300

    -Added inital commit for the visual studio gherkin extension -Added async package -Made the package autoload for debugging purposes (will be removed in the future) -Added template for a feature file -Added template for an implementation file (equivalent of steps from specflow) -Made the template for the feature file to always copy to the output directory

---
+++ Gherkin.XUnit.Quick.VS.Extension/TemplateWizzard/CopyToOutputWizard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace Gherkin.XUnit.Quick.VS.Extension.TemplateWizzard
+{
+    public class CopyToOutputWizard : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            foreach(Property p in projectItem.Properties)
+            {
+                if(p.Name == "CopyToOutputDirectory")
+                {
+                    p.Value = (uint)1;
+                }
+
+            }
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
---
+++ Gherkin.Xunit.Quick.Templates/Steps/CalculatorScenario.cs
@@ -0,0 +1,31 @@
+﻿[FeatureFile("./Calculator.feature")]
+public sealed class AddTwoNumbers : Feature
+{
+    private int _firstNumber;
+    private int _secondNumber;
+    private int _result;
+
+    [Given(@"I chose (\d+) as first number")]
+    public void I_chose_first_number(int firstNumber)
+    {
+        _firstNumber = firstNumber;
+    }
+
+    [And(@"I chose (\d+) as second number")]
+    public void I_chose_second_number(int secondNumber)
+    {
+        _secondNumber = secondNumber;
+    }
+
+    [When(@"I press add")]
+    public void I_press_add()
+    {
+        _result = _firstNumber + _secondNumber;
+    }
+
+    [Then(@"the result should be (\d+) on the screen")]
+    public void The_result_should_be_z_on_the_screen(int expectedResult)
+    {
+        Assert.Equal(expectedResult, _result);
+    }
+}
